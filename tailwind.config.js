341561ca1da7acf9a1a1bbe3a677377f517417b7
Phạm Luận <pvluan17@gmail.com>
2022-12-12 23:01:42 +0700

    fix: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ tailwind.config = {
                 primary: '#0768B5',
                 skyblue: '#2F9FF8',
                 opacity: '#072D4B',
-                mainbg: '#F4F9F8'
+                mainbg: '#F4F9F8',
+                menu: 'rgba(42, 61, 39, 0.1)',
             },
+            spacing: {
+                '4/10': '42%',
+                '0.3': '3%'
+              }
         }
     }
 }
