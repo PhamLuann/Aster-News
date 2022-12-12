54dbb5bc4b738e351bffd3203b7ba739b2f2a96e
Luanpv <pvluan17@gmail.com>
2022-12-12 17:41:12 +0700

    feat: add post

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 const menu = document.getElementById('menu')
 const menu_btn = document.getElementById('menu_btn')
-document.addEventListener('click', (e) => {
-    if (menu_btn.contains(e.target)) {
-      // Click to Toggle top menu icon
-      menu.classList.toggle('hidden')
-    } else {
-      // Click Outside from Toggle top menu icon
-      if (menu.classList.contains('tqd-topmenu-expanded')) {
-        topMenu.classList.remove('tqd-topmenu-expanded')
-        topMenu.classList.add('hidden')
-      }
+const small_logo = document.getElementById('small-logo')
+const large_logo = document.getElementById('large')
+const left_side = document.getElementById('left-side')
+document.addEventListener('click', (e)=>{
+    if (menu_btn.contains(e.target)){
+        menu.classList.toggle('hidden');
+        menu.classList.toggle('unhidden');
+        small_logo.classList.toggle('hidden')
+        large_logo.classList.add('pt-4')
+        left_side.classList.add('layer')
+    }else {
+        // Click Outside from Toggle top menu icon
+        if (menu.classList.contains('unhidden')) {
+            menu.classList.add('hidden')
+            menu.classList.remove('unhidden')
+            small_logo.classList.remove('hidden')
+            large_logo.classList.remove('pt-4')
+            left_side.classList.remove('layer')
+        }
     }
-  })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ tailwind.config = {
             colors: {
                 primary: '#0768B5',
                 skyblue: '#2F9FF8',
+                opacity: '#072D4B',
+                mainbg: '#F4F9F8'
             },
         }
     }
